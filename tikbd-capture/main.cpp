ce18dc8b70f4b4431c2a06939190945df302a8b5
Erik Piehl <epiehl@gmail.com>
2022-07-09 15:37:24 +0300

    Initial Commit

---
+++ tikbd-capture/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  tikbd-capture
+//
+//  Created by Erik Piehl on 9.7.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
